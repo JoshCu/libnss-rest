cbe3a542c5d9a4bb0e231ed1ae2c876c4ce60edd
root <josh.cu@gmail.com>
2022-06-09 00:48:57 +0100

    add group methods (segfaults)

--- mongo.c
+++ mongo.c
@@ -14,12 +14,16 @@
     fakeUser.pw_dir = "/home/fake";   /* Home directory.  */ \
     fakeUser.pw_shell = "/bin/bash";
 
+#define GROUP_DETAILS                                   \
+    fakeGroup.gr_name = "fakegroup"; /* Group name.  */ \
+    fakeGroup.gr_passwd = "x";       /* Password.    */ \
+    fakeGroup.gr_gid = 12345;        /* Group ID.    */ \
+                                     // fakeGroup.gr_mem[0] = *ptrfakeUser; /* Member list. */
+
 // POINTERS
 enum nss_status _nss_mongo_getpwnam_r(const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
     int retval;
-    char filter[1024];
-
     // initiate logging
     setlogmask(LOG_UPTO(LOG_INFO));
     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
@@ -47,8 +51,6 @@ cleanup:
 enum nss_status _nss_mongo_getpwuid_r(__uid_t uid, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
     int retval;
-    char filter[1024];
-
     // initiate logging
     setlogmask(LOG_UPTO(LOG_INFO));
     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
@@ -72,69 +74,58 @@ cleanup:
     return retval;
 }
 
-// enum nss_status _nss_mongo_getgrgid_r(__gid_t gid, struct group *result, char *buffer, size_t buflen, int *errnop)
-// {
-//     int retval;
-//     char filter[1024];
+enum nss_status _nss_mongo_getgrgid_r(__gid_t gid, struct group *result, char *buffer, size_t buflen, int *errnop)
+{
+    int retval;
+    // initiate logging
+    setlogmask(LOG_UPTO(LOG_INFO));
+    openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    syslog(LOG_INFO, "getgrgid");
+    syslog(LOG_INFO, "group_id : %d,", gid);
 
-//     // initiate logging
-//     setlogmask(LOG_UPTO(LOG_INFO));
-//     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-//     syslog(LOG_INFO, "getgrgid");
-//     syslog(LOG_INFO, "group_id : %d,", gid);
+    if (gid == 12345)
+    {
+        struct group fakeGroup;
+        GROUP_DETAILS
+        struct group *ptrfakeGroup = &fakeGroup;
+        *result = *ptrfakeGroup;
+        retval = NSS_STATUS_SUCCESS;
+        goto cleanup;
+    }
 
-//     if (gid == 12345)
-//     {
-//         syslog(LOG_INFO, "making struct");
-//         struct group fakeGroup;
-//         fakeGroup.gr_name = "fakegroup"; /* Group name.  */
-//         fakeGroup.gr_passwd = "x";       /* Password.    */
-//         fakeGroup.gr_gid = 12345;        /* Group ID.    */
-//         fakeGroup.gr_mem[0] = 1234;      /* Member list. */
+    retval = NSS_STATUS_NOTFOUND;
+cleanup:
+    closelog();
 
-//         struct group *ptrfakeGroup = &fakeGroup;
-//         *result = *ptrfakeGroup;
-//         retval = NSS_STATUS_SUCCESS;
-//         goto cleanup;
-//     }
+    return retval;
+}
 
-//     retval = NSS_STATUS_NOTFOUND;
-// cleanup:
-//     closelog();
+enum nss_status _nss_mongo_getgrnam_r(const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
+{
+    int retval;
+    char filter[1024];
 
-//     return retval;
-// }
+    // initiate logging
+    setlogmask(LOG_UPTO(LOG_INFO));
+    openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    syslog(LOG_INFO, "getgrnam");
+    syslog(LOG_INFO, "group_name : %s,", name);
+    char hardName[9] = "fakegroup";
+    int value = strcmp(name, hardName);
+    if (value == 0)
+    {
+        syslog(LOG_INFO, "making struct");
+        struct group fakeGroup;
+        GROUP_DETAILS
+        struct group *ptrfakeGroup = &fakeGroup;
+        *result = *ptrfakeGroup;
+        retval = NSS_STATUS_SUCCESS;
+        goto cleanup;
+    }
 
-// enum nss_status _nss_mongo_getgrnam_r(const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
-// {
-//     int retval;
-//     char filter[1024];
+    retval = NSS_STATUS_NOTFOUND;
+cleanup:
+    closelog();
 
-//     // initiate logging
-//     setlogmask(LOG_UPTO(LOG_INFO));
-//     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
-//     syslog(LOG_INFO, "getgrnam");
-//     syslog(LOG_INFO, "group_name : %s,", name);
-//     char hardName[9] = "fakegroup";
-//     int value = strcmp(name, hardName);
-//     if (value == 0)
-//     {
-//         syslog(LOG_INFO, "making struct");
-//         struct group fakeGroup;
-//         fakeGroup.gr_name = "fakegroup"; /* Group name.  */
-//         fakeGroup.gr_passwd = "x";       /* Password.    */
-//         fakeGroup.gr_gid = 12345;        /* Group ID.    */
-//         // fakeGroup.gr_mem[0] = 1234;      /* Member list. */
-
-//         struct group *ptrfakeGroup = &fakeGroup;
-//         *result = *ptrfakeGroup;
-//         retval = NSS_STATUS_SUCCESS;
-//         goto cleanup;
-//     }
-
-//     retval = NSS_STATUS_NOTFOUND;
-// cleanup:
-//     closelog();
-
-//     return retval;
-// }
+    return retval;
+}
