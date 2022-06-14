d2f6c4a8d6134c002c49ef1ce88550af81b6e7cf
root <josh.cu@gmail.com>
2022-06-14 19:29:54 +0100

    add api calls

--- mongo.c
+++ mongo.c
@@ -5,18 +5,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <curl/curl.h>
+#include <json-c/json.h>
 
 #define CURL_VERBOSE 0
-
-// #define USER_DETAILS                                         \
-//     syslog(LOG_INFO, "user_details");                        \
-//     fakeUser.pw_name = "fake";                               \
-//     fakeUser.pw_passwd = "password";  /* Password.  */       \
-//     fakeUser.pw_uid = (__uid_t)1234;  /* User ID.  */        \
-//     fakeUser.pw_gid = (__gid_t)12345; /* Group ID.  */       \
-//     fakeUser.pw_gecos = "fakeuser";   /* Real name.  */      \
-//     fakeUser.pw_dir = "/home/fake";   /* Home directory.  */ \
-//     fakeUser.pw_shell = "/bin/bash";
 
 #define GROUP_DETAILS                                   \
     syslog(LOG_INFO, "group_details");                  \
@@ -24,14 +15,6 @@
     fakeGroup.gr_passwd = "x";       /* Password.    */ \
     fakeGroup.gr_gid = 12345;        /* Group ID.    */ \
                                      // fakeGroup.gr_mem[0] = *ptrfakeUser; /* Member list. */
-
-#define GET_NEXT_VALUE               \
-    tmp = data;                      \
-    for (i = 0; data[i] != ':'; i++) \
-        ;                            \
-    data[i] = '\0';                  \
-    data = data + i + 1;             \
-    printf("%s\n", tmp);
 
 struct curl_output
 {
@@ -96,7 +79,6 @@ char *handle_url(char *url)
     return data.memory;
 }
 
-// POINTERS
 enum nss_status _nss_mongo_getpwnam_r(const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
     int retval;
@@ -105,50 +87,51 @@ enum nss_status _nss_mongo_getpwnam_r(const char *name, struct passwd *result, c
     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
     syslog(LOG_INFO, "getpwnam");
     syslog(LOG_INFO, "user_name : %s,", name);
-    char hardName[5] = "fake";
-    int value = strcmp(name, hardName);
-    if (value == 0)
+
+    char url[] = "127.0.0.1:8000/user/name/";
+    strcat(url, name);
+
+    struct passwd fakeUser;
+    CURL *curl;
+    char *data;
+    char *values[7];
+    CURLcode res;
+    curl = curl_easy_init();
+    if (curl)
     {
-        struct passwd fakeUser;
-        CURL *curl;
-        char *data;
-        CURLcode res;
-        curl = curl_easy_init();
-        if (curl)
+        data = handle_url(url);
+        syslog(LOG_INFO, "response: %s", data);
+        if (data)
         {
-            data = handle_url("127.0.0.1:8000/user/name/fake");
-            syslog(LOG_INFO, "response: %s", data);
-            if (data)
+            int i = 0;
+            int counter = 0;
+            char *start = data;
+            for (i = 0; data[i] != '\0'; i++)
             {
-                int i = 0;
-                char *tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_name = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_passwd = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_uid = (__uid_t)atoi(tmp);
-                GET_NEXT_VALUE
-                fakeUser.pw_gid = (__gid_t)atoi(tmp);
-                GET_NEXT_VALUE
-                fakeUser.pw_gecos = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_dir = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_shell = tmp;
+                if (data[i] == ':')
+                {
+                    data[i] = '\0';
+                    // printf("%s\n", start);
+                    values[counter] = start;
+                    start = &data[i + 1];
+                    counter++;
+                }
             }
-            /* example.com is redirected, so we tell libcurl to follow redirection */
-            /* Perform the request, res will get the return code */
+            fakeUser.pw_name = values[0];
+            fakeUser.pw_passwd = values[1];
+            fakeUser.pw_uid = (__uid_t)atoi(values[2]);
+            fakeUser.pw_gid = (__gid_t)atoi(values[3]);
+            fakeUser.pw_gecos = values[4];
+            fakeUser.pw_dir = values[5];
+            fakeUser.pw_shell = values[6];
         }
-
-        /* always cleanup */
-        // curl_easy_cleanup(curl);
-
-        struct passwd *ptrfakeUser = &fakeUser;
-        *result = *ptrfakeUser;
-        retval = NSS_STATUS_SUCCESS;
-        goto cleanup;
+        /* example.com is redirected, so we tell libcurl to follow redirection */
+        /* Perform the request, res will get the return code */
     }
+    struct passwd *ptrfakeUser = &fakeUser;
+    *result = *ptrfakeUser;
+    retval = NSS_STATUS_SUCCESS;
+    goto cleanup;
 
     retval = NSS_STATUS_NOTFOUND;
 cleanup:
@@ -166,49 +149,51 @@ enum nss_status _nss_mongo_getpwuid_r(__uid_t uid, struct passwd *result, char *
     syslog(LOG_INFO, "getpwnuid");
     syslog(LOG_INFO, "user_id : %d,", uid);
 
-    if (uid == 1234)
+    char struid[50];
+    sprintf(struid, "%d", uid);
+    char url[] = "127.0.0.1:8000/user/id/";
+    strcat(url, struid);
+
+    struct passwd fakeUser;
+    CURL *curl;
+    char *data;
+
+    CURLcode res;
+    char *values[7];
+    curl = curl_easy_init();
+    if (curl)
     {
-        struct passwd fakeUser;
-        CURL *curl;
-        char *data;
-        CURLcode res;
-        curl = curl_easy_init();
-        if (curl)
+        data = handle_url(url);
+        syslog(LOG_INFO, "response: %s", data);
+        if (data)
         {
-            data = handle_url("127.0.0.1:8000/user/name/1234");
-            syslog(LOG_INFO, "response: %s", data);
-            if (data)
+            int i = 0;
+            int counter = 0;
+            char *start = data;
+            for (i = 0; data[i] != '\0'; i++)
             {
-                int i = 0;
-                char *tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_name = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_passwd = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_uid = (__uid_t)atoi(tmp);
-                GET_NEXT_VALUE
-                fakeUser.pw_gid = (__gid_t)atoi(tmp);
-                GET_NEXT_VALUE
-                fakeUser.pw_gecos = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_dir = tmp;
-                GET_NEXT_VALUE
-                fakeUser.pw_shell = tmp;
+                if (data[i] == ':')
+                {
+                    data[i] = '\0';
+                    // printf("%s\n", start);
+                    values[counter] = start;
+                    start = &data[i + 1];
+                    counter++;
+                }
             }
-            /* example.com is redirected, so we tell libcurl to follow redirection */
-            /* Perform the request, res will get the return code */
-            // res = curl_easy_perform(curl);
+            fakeUser.pw_name = values[0];
+            fakeUser.pw_passwd = values[1];
+            fakeUser.pw_uid = (__uid_t)atoi(values[2]);
+            fakeUser.pw_gid = (__gid_t)atoi(values[3]);
+            fakeUser.pw_gecos = values[4];
+            fakeUser.pw_dir = values[5];
+            fakeUser.pw_shell = values[6];
         }
-
-        /* always cleanup */
-        // curl_easy_cleanup(curl);
-
-        struct passwd *ptrfakeUser = &fakeUser;
-        *result = *ptrfakeUser;
-        retval = NSS_STATUS_SUCCESS;
-        goto cleanup;
     }
+    struct passwd *ptrfakeUser = &fakeUser;
+    *result = *ptrfakeUser;
+    retval = NSS_STATUS_SUCCESS;
+    goto cleanup;
 
     retval = NSS_STATUS_NOTFOUND;
 cleanup:
@@ -226,15 +211,46 @@ enum nss_status _nss_mongo_getgrgid_r(__gid_t gid, struct group *result, char *b
     syslog(LOG_INFO, "getgrgid");
     syslog(LOG_INFO, "group_id : %d,", gid);
 
-    if (gid == 12345)
+    char struid[50];
+    sprintf(struid, "%d", gid);
+    char url[] = "127.0.0.1:8000/group/id/";
+    strcat(url, struid);
+
+    struct group fakeGroup;
+    CURL *curl;
+    char *data;
+    CURLcode res;
+    char *values[3];
+    curl = curl_easy_init();
+    if (curl)
     {
-        struct group fakeGroup;
-        GROUP_DETAILS
-        struct group *ptrfakeGroup = &fakeGroup;
-        *result = *ptrfakeGroup;
-        retval = NSS_STATUS_SUCCESS;
-        goto cleanup;
+        data = handle_url(url);
+        syslog(LOG_INFO, "response: %s", data);
+        if (data)
+        {
+            int i = 0;
+            int counter = 0;
+            char *start = data;
+            for (i = 0; data[i] != '\0'; i++)
+            {
+                if (data[i] == ':')
+                {
+                    data[i] = '\0';
+                    // printf("%s\n", start);
+                    values[counter] = start;
+                    start = &data[i + 1];
+                    counter++;
+                }
+            }
+            fakeGroup.gr_name = values[0];
+            fakeGroup.gr_passwd = values[1];
+            fakeGroup.gr_gid = (__gid_t)atoi(values[2]);
+        }
     }
+    struct group *ptrfakeGroup = &fakeGroup;
+    *result = *ptrfakeGroup;
+    retval = NSS_STATUS_SUCCESS;
+    goto cleanup;
 
     retval = NSS_STATUS_NOTFOUND;
 cleanup:
@@ -253,17 +269,45 @@ enum nss_status _nss_mongo_getgrnam_r(const char *name, struct group *result, ch
     openlog("mongo_nss", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
     syslog(LOG_INFO, "getgrnam");
     syslog(LOG_INFO, "group_name : %s,", name);
-    int value = strcmp(name, "fakegroup");
-    syslog(LOG_INFO, "compare_val : %d,", value);
-    if (value == 0)
+
+    char url[] = "127.0.0.1:8000/group/name/";
+    strcat(url, name);
+
+    struct group fakeGroup;
+    CURL *curl;
+    char *data;
+    CURLcode res;
+    char *values[3];
+    curl = curl_easy_init();
+    if (curl)
     {
-        struct group fakeGroup;
-        GROUP_DETAILS
-        struct group *ptrfakeGroup = &fakeGroup;
-        *result = *ptrfakeGroup;
-        retval = NSS_STATUS_SUCCESS;
-        goto cleanup;
+        data = handle_url("127.0.0.1:8000/group/name/fakegroup");
+        syslog(LOG_INFO, "response: %s", data);
+        if (data)
+        {
+            int i = 0;
+            int counter = 0;
+            char *start = data;
+            for (i = 0; data[i] != '\0'; i++)
+            {
+                if (data[i] == ':')
+                {
+                    data[i] = '\0';
+                    // printf("%s\n", start);
+                    values[counter] = start;
+                    start = &data[i + 1];
+                    counter++;
+                }
+            }
+            fakeGroup.gr_name = values[0];
+            fakeGroup.gr_passwd = values[1];
+            fakeGroup.gr_gid = (__gid_t)atoi(values[2]);
+        }
     }
+    struct group *ptrfakeGroup = &fakeGroup;
+    *result = *ptrfakeGroup;
+    retval = NSS_STATUS_SUCCESS;
+    goto cleanup;
 
     retval = NSS_STATUS_NOTFOUND;
 cleanup:
